d407fe02df0f28f0e2cf49a1a16dd1cf8bc8340e
yolokbrother <www.mineman198@gmail.com>
2022-12-31 19:07:31 +0800

    bottom bar

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
     }
 }
