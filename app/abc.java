d1b0ace44ff71ba22f06700a0627f94e2d97aad4
yolokbrother <www.mineman198@gmail.com>
2023-01-11 04:32:25 +0800

    voice recognition

---
+++ app/abc.java
@@ -0,0 +1,16 @@
+public class abc {
+    public void speak(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Start Speaking");
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            textView.setText(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS).get(0));
+        }
+}
--- app/src/main/java/com/example/myapplication/SearchImageActivity.kt
+++ app/src/main/java/com/example/myapplication/SearchImageActivity.kt
@@ -1,14 +1,22 @@
 package com.example.myapplication
 
+import android.app.Activity
 import android.content.Intent
 import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.view.get
 import com.example.myapplication.databinding.ActivitySearchImageBinding
 import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_search_image.*
+import kotlinx.android.synthetic.main.activity_search_image.bottomNavigationView
+import kotlinx.android.synthetic.main.activity_search_image.fab
 import java.io.File
+import java.util.Locale
 
 class SearchImageActivity : AppCompatActivity() {
 
@@ -19,7 +27,27 @@ class SearchImageActivity : AppCompatActivity() {
         //bottom navigation
         binding = ActivitySearchImageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        bottomNavigationView.background = null
+        bottomNavigationView.menu[2].isEnabled = false
+        //bottom navigation//selected
+        bottomNavigationView.selectedItemId = R.id.miHome
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.miHome -> startActivity(Intent(this, MainActivity::class.java))
+                R.id.miProfile -> startActivity(Intent(this, ViewPersonalActivity::class.java))
+                R.id.miSearch -> startActivity(Intent(this, SearchImageActivity::class.java))
+                R.id.miSettings -> startActivity(Intent(this, SettingActivity::class.java))
+            }
+            true
+        }
 
+        fab.setOnClickListener {
+            startActivity(Intent(this, AddPhotoActivity::class.java))
+        }
+
+
+
+        //get image from firebase
         binding.getImage.setOnClickListener{
             val imageName = binding.etImageId.text.toString()
             val storageRef = FirebaseStorage.getInstance().reference.child("uploads/$imageName.jpg")
@@ -37,6 +65,7 @@ class SearchImageActivity : AppCompatActivity() {
             }
         }
 
+        //upload image from firebase to cloud
         shareImage.setOnClickListener{
             val myIntent = Intent(Intent.ACTION_SEND)
             myIntent.type = "type/palin"
@@ -45,6 +74,32 @@ class SearchImageActivity : AppCompatActivity() {
             myIntent.putExtra(Intent.EXTRA_SUBJECT,shareBody)
             myIntent.putExtra(Intent.EXTRA_TEXT,shareSub)
             startActivity(Intent.createChooser(myIntent,"Share your picture"))
+        }
+
+        //speak to text
+        speakText.setOnClickListener{
+            askSpeechInput()
+        }
+    }
+
+    var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            // There are no request codes
+            val data: Intent? = result.data
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            etImageId.setText(result?.get(0).toString())
+        }
+    }
+
+    private fun askSpeechInput() {
+        if (!SpeechRecognizer.isRecognitionAvailable(this)){
+            Toast.makeText(this,"Speech recognition not available",Toast.LENGTH_SHORT).show()
+        } else{
+            val i = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE,Locale.getDefault())
+            i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say the picture you want to find")
+            resultLauncher.launch(i)
         }
     }
 }
